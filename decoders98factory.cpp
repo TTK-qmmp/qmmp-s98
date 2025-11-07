cb85a0cef0e383c33b4c3bf00aa166daff672f4e
Greedysky <greedysky@163.com>
2025-11-07 15:21:59 +0800

    feat: Support for qmmp 0.x Qt4

--- decoders98factory.cpp
+++ decoders98factory.cpp
@@ -103,3 +103,8 @@ QString DecoderS98Factory::translation() const
 {
     return QString();
 }
+
+#if QT_VERSION < QT_VERSION_CHECK(5,0,0)
+#include <QtPlugin>
+Q_EXPORT_PLUGIN2(s98, DecoderS98Factory)
+#endif
