e902e6048bbbea011f6f650192fe50b237ab75a3
Greedysky <greedysky@163.com>
2025-12-04 14:29:12 +0800

    refactor: Clean code

--- libs98/device/s98types.h
+++ libs98/device/s98types.h
@@ -3,18 +3,18 @@
 
 #define __inline	__inline__
 #ifndef NEVER_REACH
-#define NEVER_REACH
+# define NEVER_REACH
 #endif
 
-typedef int				Int;
+typedef int             Int;
 typedef unsigned int	Uint;
-typedef signed int		Int32;
+typedef signed int      Int32;
 typedef unsigned int	Uint32;
 typedef signed short	Int16;
 typedef unsigned short	Uint16;
-typedef signed char		Int8;
+typedef signed char     Int8;
 typedef unsigned char	Uint8;
-typedef char			Char;
+typedef char            Char;
 
 typedef Int32 Sample;
 
@@ -22,10 +22,10 @@ typedef Int32 Sample;
 #include <stdlib.h>
 #include <memory.h>
 
-#define XSLEEP(t)	sleep(t)
-#define XMALLOC(s)	malloc(s)
+#define XSLEEP(t)       sleep(t)
+#define XMALLOC(s)      malloc(s)
 #define XREALLOC(p,s)	realloc(p,s)
-#define XFREE(p)	free(p)
+#define XFREE(p)        free(p)
 #define XMEMCPY(d,s,n)	memcpy(d,s,n)
 #define XMEMSET(d,c,n)	memset(d,c,n)
 
